71dfd4296f85ab1400700c54eb1de28d83c04df0
Sebastian Mauer <mauimauer@gmail.com>
2014-02-03 19:49:41 +0100

    Properly close connection

--- template/server.js
+++ template/server.js
@@ -2,5 +2,5 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.write('Version: ' + JSON.stringify(process.versions, 0, 2)+ '\n');
-  res.write('This is a new GDG[x] NodeJS Cartridge on '+process.env.OPENSHIFT_APP_DNS+'\n');
+  res.end('This is a new GDG[x] NodeJS Cartridge on '+process.env.OPENSHIFT_APP_DNS+'\n');
 }).listen(process.env.OPENSHIFT_NODEJS_PORT, process.env.OPENSHIFT_NODEJS_IP);
